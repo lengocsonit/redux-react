ef8167b80e449c8e06b9ae1096484aff4fd8957a
LE NGOC SON <63851028+lengocsonit@users.noreply.github.com>
2020-05-25 06:30:08 +0900

    Chia tach reducer

--- src/Store.js
+++ src/Store.js
@@ -24,12 +24,39 @@ var reducer1 = (state = oldState, action) => {
     return state
 }
 
+//Chia tach reducder de quan ly
+const numReducerInitialState = [1, 2, 3]
+const numReducer = (state = numReducerInitialState, action) => {
+    switch (action.type) {
+        case "ADD_NUM":
+            return [...state, action.newItem]
+        case "DELETE":
+            return state.filter((value, index) => value !== action.num)
+        default:
+            return state
+    }
+}
+
+const editStatusReducerInitialState = true
+const editStatusReducer = (state = editStatusReducerInitialState, action) => {
+    switch (action.type) {
+        case "EDIT_STATE":
+            return !state
+        default:
+            return state
+    }
+}
+//Ket hoi cac reducer lai thanh 1 reducer
+const allReducer = redux.combineReducers({
+    num: numReducer,
+    editStatus: editStatusReducer
+})
+
 //Khoi tao store1 quan ly reducer1
-var store1 = redux.createStore(reducer1)
+var store1 = redux.createStore(allReducer)
 
 //Moi khi state thay doi, thi thuc hien
 store1.subscribe(() => {
-    console.log("state changed")
     console.log(store1.getState())
 })
 
@@ -40,7 +67,7 @@ store1.dispatch({
 
 store1.dispatch({
     type: "ADD_NUM",
-    newItem: 10
+    newItem: 20
 })
 
 store1.dispatch({
